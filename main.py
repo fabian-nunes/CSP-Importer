312cecfc16391f385deb03d730b9658b55fb2071
fabian-nunes <fabiannunes12@gmail.com>
2023-07-17 20:56:42 +0100

    Changed mongo for sqlite since raspberry is a bitch

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import json
 import os
 import re
+import sqlite3
 
 import pytesseract
 from bson import json_util
 from flask import Flask, request
-from pymongo import MongoClient
 
 #pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 app = Flask(__name__)
 
-# MongoDB configuration
-client = MongoClient('mongodb://localhost:27017/')
-db = client['cecotec']
-collection = db['values']
-
-
 def process_image(image):
     # text dictionary
     text_lan = ["Peso", "IMC", "% Gordura", "Gordura subcutanea", "Agua do corpo", "Musculo esquelético",
                 "Massa Muscular", "massa Ossea", "% Proteina", "TMB", "Idade do Corpo"]
+    text_db = ["weight", "bmi", "fat", "subcutaneous_fat", "body_water", "skeletal_muscle", "muscle_mass", "bone_mass",
+               "protein", "bmr", "body_age"]
     # Save the image to a temporary location
     image_path = 'temp_image.jpg'
     image.save(image_path)
@@ -29,25 +25,33 @@ def process_image(image):
     extracted_text = pytesseract.image_to_string(image_path)
 
     values = {}
+    bone_mass = 0
     for line in extracted_text.split("\n"):
         # check if line contains date
-        if re.search(r"\d{2}/\d{2}/\d{4}", line):
-            values["Date"] = line
-            # check date in database
-            date = line.split()[1]
-            query = {'Date': {'$regex': date}}
-            existing_record = collection.find_one(query)
-            if existing_record:
-                # date already exists in database
-                return 'Already uploaded a scale for this date', 400
-        for label in text_lan:
-            if label in line:
-                value = re.search(r"\d+(\.\d+)?", line)
-                if value:
-                    values[label] = value.group()
+        with sqlite3.connect('database.db') as sqlite_conn:
+            sqlite_cursor = sqlite_conn.cursor()
+            if re.search(r"\d{2}/\d{2}/\d{4}", line):
+                values["Date"] = line
+                # check date in database
+                date = line.split()[1]
+                sqlite_cursor.execute("SELECT * FROM scale WHERE date = ?", (date,))
+                if sqlite_cursor.fetchone():
+                    return "Date already exists", 400
+            for label in text_lan:
+                if label in line:
+                    value = re.search(r"\d+(\.\d+)?", line)
+                    if value:
+                        if text_db[text_lan.index(label)] == "bone_mass":
+                            bone_mass = float(value.group())
+                        values[text_db[text_lan.index(label)]] = float(value.group())
 
     # Insert the values into the database
-    collection.insert_one(values)
+    sqlite_cursor.execute("INSERT INTO scale (date, weight, bmi, fat, subcutaneous_fat, body_water, skeletal_muscle, "
+                          "muscle_mass, bone_mass, protein, bmr, body_age) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, "
+                          "?)", (date, values["weight"], values["bmi"], values["fat"], values["subcutaneous_fat"],
+                                 values["body_water"], values["skeletal_muscle"], values["muscle_mass"],
+                                 bone_mass, values["protein"], values["bmr"], values["body_age"]))
+    sqlite_conn.commit()
     # Remove the temporary image
     os.remove(image_path)
     return values
@@ -68,6 +72,16 @@ def scale():
         values = collection.find()
         # Return the values as JSON
         return json.loads(json_util.dumps(values)), 200
+
+@app.route("/create", methods=["GET"])
+def create():
+    #Create table
+    with sqlite3.connect('database.db') as sqlite_conn:
+        sqlite_cursor = sqlite_conn.cursor()
+        sqlite_cursor.execute('''CREATE TABLE IF NOT EXISTS scale
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, date text, weight real, bmi real, fat real, subcutaneous_fat real, body_water real, skeletal_muscle real, muscle_mass real, bone_mass real, protein real, bmr real, body_age real)''')
+        sqlite_conn.commit()
+        return "Table created successfully", 200
 
 if __name__ == "__main__":
     app.run(debug=True)
